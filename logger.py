b1ea56660f931efe352b79b5311cb626358a4b0c
Nguyen Tuan Kiet <nguyentuankiet23031998@gmail.com>
2019-07-24 10:58:30 +0900

    fix log

--- logger.py
+++ logger.py
@@ -4,7 +4,7 @@ LOG_BEHAVIOR = 1
 LOG_FACIAL = 2
 LOG_MOVEMENT = 3
 
-log_files = ["behaviors", "facial", "movement"]
+log_files = ["frames", "behaviors", "facial", "movement"]
 
 
 def write_log(log_type_id, sess_id, progress, total):
